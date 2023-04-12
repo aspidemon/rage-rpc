b965eee8585de5f3a28b0fa3d020aaba392f2b62
aspidemon <96891562+aspidemon@users.noreply.github.com>
2023-04-12 14:16:01 +0300

    0.2.2-N.1

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Remote Procedure Calls for RAGE Multiplayer
 # NodeJS Interpritation
 
+## Install
+`npm i aspidemon/rage-rpc#nodejs`
+
 ## License
 MIT License
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const utils = require('./utils');
+const util = require('./utils');
 class RPC {
-    _listeners = new Map();
-    _pendings = new Map();
-    _callServer;
+    #_listeners = new Map();
+    #_pendings = new Map();
+    #_callServer;
     constructor() {
         switch (util.getEnvriroment()) {
             case 'client': {
@@ -12,14 +12,14 @@ class RPC {
                     return mp.console.logError('[RPC PROTECTION] К вашему сожалению разработчики этого сервера поступили умно использовав систему процедурных вызовов ивентов и теперь в ней не доступна возможность использовать читы типа executor');
                 };
                 mp.events.add(util.getHash('rpc.client:events:emit'), (eventName, ...args) => {
-                    let _event = this._listeners.get(eventName);
+                    let _event = this.#_listeners.get(eventName);
                     if (_event !== undefined)
                         _event(...args);
                     else
                         mp.console.logError(`[RPC] Название ивента не было найдено (EventName: ${eventName})`);
                 });
                 mp.events.add(util.getHash('rpc.cef:to:server:events:emit'), (eventName, ...args) => {
-                    this._callServer(util.getHash('rpc.server:events:emit'), eventName, ...args);
+                    this.#_callServer(util.getHash('rpc.server:events:emit'), eventName, ...args);
                 });
                 mp.events.add(util.getHash('rpc.client:to:cef:events:emit'), (browserID, eventName, ...args) => {
                     let _browser = mp.browsers.at(browserID);
@@ -29,16 +29,16 @@ class RPC {
                         mp.console.logError(`[RPC] Браузер не был найден, возможно его не существует (ID: ${browserID}, EventName: ${eventName})`);
                 });
                 mp.events.add(util.getHash(`rpc.client:events:emitProc`), (eventName, ...args) => {
-                    let _event = this._listeners.get(eventName);
+                    let _event = this.#_listeners.get(eventName);
                     if (_event !== undefined) {
                         let _result = _event(...args);
-                        this._callServer(util.getHash('rpc.server:pendings:emit'), eventName, _result);
+                        this.#_callServer(util.getHash('rpc.server:pendings:emit'), eventName, _result);
                     }
                     else
                         mp.console.logError(`[RPC] Название ивента не было найдено (EventName: ${eventName})`);
                 });
                 mp.events.add(util.getHash('rpc.client:pendings:emit'), (eventName, result) => {
-                    let _event = this._pendings.get(eventName);
+                    let _event = this.#_pendings.get(eventName);
                     if (_event !== undefined)
                         _event.resolve(result);
                     else
@@ -47,14 +47,14 @@ class RPC {
             }
             case 'server': {
                 mp.events.add(util.getHash('rpc.server:events:emit'), (player, eventName, ...args) => {
-                    let _event = this._listeners.get(eventName);
+                    let _event = this.#_listeners.get(eventName);
                     if (_event !== undefined)
                         _event(player, ...args);
                     else
                         console.log(`[RPC] Название ивента не было найдено (EventName: ${eventName})`);
                 });
                 mp.events.add(util.getHash('rpc.server:events:emitProc'), (player, eventName, ...args) => {
-                    let _event = this._listeners.get(eventName);
+                    let _event = this.#_listeners.get(eventName);
                     if (_event !== undefined) {
                         let _result = _event(player, ...args);
                         player.call(util.getHash('rpc.client:pendings:emit'), [eventName, _result]);
@@ -63,7 +63,7 @@ class RPC {
                         console.log(`[RPC] Название ивента не было найдено (EventName: ${eventName})`);
                 });
                 mp.events.add(util.getHash('rpc.server:pendings:emit'), (player, eventName, result) => {
-                    let _event = this._pendings.get(eventName);
+                    let _event = this.#_pendings.get(eventName);
                     if (_event !== undefined)
                         _event.resolve(result);
                     else
@@ -72,49 +72,49 @@ class RPC {
             }
         }
     }
-    _has(eventName) {
+    #_has(eventName) {
         return this._listeners.has(eventName);
     }
-    _on(eventName, handler) {
-        if (!this._has(eventName))
+    #_on(eventName, handler) {
+        if (!this.#_has(eventName))
             this._listeners.set(eventName, handler);
     }
-    async _emitProc(eventName, ...args) {
+    async #_emitProc(eventName, ...args) {
         return new Promise(resolve => {
             let _result = this._listeners.get(eventName)(...args);
             resolve(_result);
         });
     }
-    _off(eventName) {
-        if (this._has(eventName))
+    #_off(eventName) {
+        if (this.#_has(eventName))
             this._listeners.delete(eventName);
     }
-    _emit(eventName, ...args) {
-        if (this._has(eventName))
+    #_emit(eventName, ...args) {
+        if (this.#_has(eventName))
             this._listeners.get(eventName)(...args);
     }
     on(eventName, handler) {
-        this._on(eventName, handler);
+        this.#_on(eventName, handler);
     }
     off(eventName) {
-        this._off(eventName);
+        this.#_off(eventName);
     }
     emit(eventName, ...args) {
-        this._emit(eventName, ...args);
+        this.#_emit(eventName, ...args);
     }
     async emitProc(eventName, ...args) {
-        return await this._emitProc(eventName, ...args);
+        return await this.#_emitProc(eventName, ...args);
     }
     onServerProc(eventName, handler) {
-        this._on(eventName, handler);
+        this.#_on(eventName, handler);
     }
     emitServer(eventName, ...args) {
-        this._callServer(util.getHash('rpc.server:events:emit'), eventName, ...args);
+        this.#_callServer(util.getHash('rpc.server:events:emit'), eventName, ...args);
     }
     async emitServerProc(eventName, ...args) {
         return new Promise((res) => {
             this._pendings.set(eventName, { resolve: res });
-            this._callServer(util.getHash('rpc.server:events:emitProc'), eventName, ...args);
+            this.#_callServer(util.getHash('rpc.server:events:emitProc'), eventName, ...args);
         });
     }
     emitClient(player, eventName, ...args) {
@@ -124,7 +124,7 @@ class RPC {
         mp.players.call(util.getHash('rpc.client:events:emit'), [eventName, ...args]);
     }
     onClientProc(eventName, handler) {
-        this._on(eventName, handler);
+        this.#_on(eventName, handler);
     }
     async emitClientProc(player, eventName, ...args) {
         return new Promise((res) => {
